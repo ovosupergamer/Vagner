01efa0f5fcbfee28319829ffb9230aa63e1edbaa
ovosupergamer <gulorenzettitga@gmail.com>
2026-03-05 19:06:47 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,20 +14,5 @@
 
 int main()
 {
-	auto Sun = EngineReflexion::ReflectMethod<double,double,double>("Sun",{EngineReflexion::Type_Double,EngineReflexion::Type_Double},EngineReflexion::Type_Double,[](double First, double Second)
-	{
-		return First + Second;
-	});
-
-	auto Event = EngineReflexion::ReflectMethod<void,std::string>("Event",{EngineReflexion::Type_StdString},EngineReflexion::Type_Void,[](std::string Message)
-	{
-		std::cout << Message << "\n";
-	});
-
-	std::cout << typeid(Event).name() << "\n";
-	std::cout << typeid(Sun).name() << "\n";
-	std::cout << Sun.ExecuteFunctionWithReturn(25,25) << "\n";
-	Event.ExecuteFunctionWithNoReturn("cu");
-
 	return 0;
 }
